1fbf34e8b9d7fbf457dc79b1c55f8030aacabcb5
hopo <hpsight@gmail.com>
2017-02-07 15:15:04 +0900

    o2_IO_2

---
+++ Input_Output/1.py
@@ -0,0 +1,9 @@
+in_str=input("INPUT: ")
+print(in_str.upper()+", Hello!!!")
+
+
+"""
+puts("INPUT: ")
+in_str=gets.chomp()
+puts(in_str.upcase()+", Hello!!!")
+"""
---
+++ Input_Output/2.py
@@ -0,0 +1,21 @@
+in_str=input("INPUT: ")
+idA="11";idB="bb"
+if in_str==idA:
+	print("Hello. A")
+elif in_str==idB:
+	print("Hello. B")
+else:
+	print("WHO???")
+
+"""
+puts("INPUT: ")
+in_str=gets.chomp()
+idA="11";idB="bb"
+if in_str==idA
+	puts("Hello! A")
+elseif in_str==idB
+	puts("Hello! B")
+else
+	puts("WHO???")
+end
+"""
