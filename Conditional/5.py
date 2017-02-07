2427eb2a6003d25694be38b48daaedd47d0c54e3
hp <HPMBA@HPMBA.local>
2017-02-07 14:26:16 +0900

    new files C/2.py 5.py

---
+++ Conditional/5.py
@@ -0,0 +1,20 @@
+id="aa"
+in_A=11;in_B="aa"
+if id==in_A:
+	print("Hello. A")
+elif id==in_B:
+	print("Hello. B")
+else:
+	print("WHO???")
+
+"""
+id=11
+in_A=11;in_B="aa"
+if id==in_A
+	puts("Hello! A")
+elseif id==in_B
+	puts("Hello! B")
+else
+	puts("WHO???")
+end
+"""
