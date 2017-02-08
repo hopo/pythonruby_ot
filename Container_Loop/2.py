f4e4992735d4b6481a750898b5b4b32780eebf03
hopo <hpsight@gmail.com>
2017-02-09 00:07:47 +0900

    add 2.py

---
+++ Container_Loop/2.py
@@ -0,0 +1,10 @@
+members=['harpa','amber','riceboy','sooki']
+
+# i=0
+# while i<len(members): #R: while i<members.length do
+# 	print(members[i])
+# 	i=i+1
+# print('* while term *')
+
+for member in members:
+	print(member)
