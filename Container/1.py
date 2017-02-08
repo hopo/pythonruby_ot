1d418574636722ac23e9a7c97f4a930bcb983557
hopo <hpsight@gmail.com>
2017-02-08 19:59:37 +0900

    mkdir Container/

---
+++ Container/1.py
@@ -0,0 +1,11 @@
+name='harpa'
+names=['harpa','obama','riceboy']
+
+print(type(name)) #string #R puts(name.class)
+print(name)
+
+print(type(names)) #list
+print(names)
+
+print(names[1]) #2nd element(index1) in elements
+
---
+++ Container/2.py
@@ -0,0 +1,12 @@
+al=['A','B','C']
+
+print(al)
+print(len(al))
+
+al.append('D')
+print(al)
+print(len(al))
+
+del(al[0])
+print(al)
+print(len(al))
