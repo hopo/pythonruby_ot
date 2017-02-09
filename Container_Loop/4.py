1e571838c7617872634ba3feb618f3e21dac02d7
hp <HPMBA@HPMBA.local>
2017-02-09 14:08:27 +0900

    container and for

---
+++ Container_Loop/4.py
@@ -0,0 +1,8 @@
+inpt_id=input('ID : ')
+members=['harpa','amber']
+for member in members:
+    if member==inpt_id:
+        print('Hello world, '+member+'.')
+        import sys #R: exit
+        sys.exit()
+print('no user id.')
