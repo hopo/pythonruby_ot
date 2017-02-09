4cd9db847f99c9a9dc7c252fbb91d74b8c7dd89e
hopo <hpsight@gmail.com>
2017-02-09 14:57:05 +0900

    def

---
+++ Function/1.py
@@ -0,0 +1,15 @@
+#define function a3
+def a3 ():
+	print('aaa')
+
+#call function a3
+a3()
+
+
+"""
+#R:
+def a3()
+	puts('aaa')
+end
+a3()
+"""
---
+++ Function/2.py
@@ -0,0 +1,7 @@
+def a3():
+	a='aaa'
+	print('before')
+	return a
+	print('after')
+print(a3())
+
