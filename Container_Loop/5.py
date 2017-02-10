51b497e6d8012d0adbd608a4c6273841afdd4cc0
hopo <hpsight@gmail.com>
2017-02-10 16:15:50 +0900

    aaa

---
+++ Container_Loop/5.py
@@ -0,0 +1,15 @@
+inpt_id=input('ID: ')
+
+def login(_id):
+	members=['harpa','amber','riceboy']
+	for member in members:
+		if member==_id:
+			return True
+	return False
+
+if login(inpt_id):
+	print('Hello, '+inpt_id+'.')
+else:
+	print('no user id.')
+
+print('* app5 term *')
