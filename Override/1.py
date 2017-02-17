226fc61ace5394bef640d45ad2debafa2887268e
hopo <hpsight@gmail.com>
2017-02-17 15:39:57 +0900

    Override/

---
+++ Override/1.py
@@ -0,0 +1,11 @@
+class C1:
+	def m(self):
+		return 'Parent'
+
+class C2(C1):
+	def m(self):
+		return super().m()+ 'Child'
+	pass
+
+o=C2()
+print(o.m())
--- Override/2.py
+++ Override/2.py
@@ -27,25 +27,31 @@ class Cal(object):
     def history(cls):
         for item in Cal._history:
             print(item)
+    def info(self):
+        return 'Cal => v1 : %d, v2 : %d' % (self.v1, self.v2)
 
 class CalMultiply(Cal):
     def multiply(self):
         result=self.v1*self.v2
         Cal._history.append('mul : %d*%d=%d' % (self.v1,self.v2,result))
         return result
+    def info(self):
+        return 'CalMultiply => %s' % super().info() # super()=Cal()
+
 class CalDivide(CalMultiply):
     def divide(self):
         result=self.v1/self.v2
         Cal._history.append('div : %d/%d=%d' % (self.v1,self.v2,result))
         return result
+    def info(self):
+        return 'CalDivide => %s' % super().info() # super()=CalMutiply()
+
+
+c0=Cal(30,60)
+print(c0.info())
 
 c1=CalMultiply(10,10)
-print('c1',c1.add())
-print('c1',c1.multiply())
+print(c1.info())
 
 c2=CalDivide(20,10)
-print('c2',c2.add())
-print('c2',c2.multiply())
-print('c2',c2.divide())
-
-Cal.history()
+print(c2.info())
