e310f0843dd54e7e5ed84f149effd27c2b83be10
hopo <hpsight@gmail.com>
2018-05-21 23:06:27 +0900

    PAlgo lv2 python

--- ProgrammersAlgo/02_Level2/134_jaden_case/py-01/main.py
+++ ProgrammersAlgo/02_Level2/134_jaden_case/py-01/main.py
@@ -1,16 +1,22 @@
-# https://programmers.co.kr/learn/challenge_codes/134
-
-"""
-Jaden_Case함수는 문자열 s을 매개변수로 입력받습니다.
-s에 모든 단어의 첫 알파벳이 대문자이고, 그 외의 알파벳은 소문자인 문자열을 리턴하도록 함수를 완성하세요
-예를들어 s가 "3people unFollowed me for the last week"라면 "3people Unfollowed Me For The Last Week"를 리턴하면 됩니다.
-"""
 
 def Jaden_Case(s):
-    for x in s:
-        print(x)
-    return s
+    ret = ''
+    flag = True
+    cli = s.lower()
+
+    for x in cli:
+        if x == ' ':
+            flag = True
+        elif flag:
+            flag = False
+            x = x.upper()
+        ret += x
+        
+    return ret
 
 if __name__ == '__main__':
     ex1 = Jaden_Case("3people unFollowed me for the last week") # 3people Unfollowed Me For The Last Week
     print(ex1)
+
+    ex2 = Jaden_Case("Press ENTER or type command to continue") # Press Enter Or Type Command To Continue
+    print(ex2)
