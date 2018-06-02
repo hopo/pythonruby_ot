d6f6394f7fec5f74efa6999166a49df0ee73083e
hopo <hpsight@gmail.com>
2018-06-02 17:54:40 +0900

    CIO ho py

--- CheckIO/02_Home/02_non-unique-elements/go-01/main.go
+++ CheckIO/02_Home/02_non-unique-elements/go-01/main.go
@@ -23,18 +23,3 @@ func main() {
 	fmt.Println(nonUniqueElements([]int{5, 5, 5, 5, 5}))       //[5, 5, 5, 5, 5], "3rd example"
 	fmt.Println(nonUniqueElements([]int{10, 9, 10, 10, 9, 8})) //[10, 9, 10, 10, 9], "4th example"
 }
-
-/*
-function nonUniqueElements(data) {
-	for(var i=0; i<data.length; i++) {
-		var sock = data[i];
-		delete data[i];
-		data.includes(sock) ? data[i] = sock : null;
-	};
-	return data.filter(() => {return true});
-}
-*/
-
-/*
-nonUniqueElements = a => a.filter(e => a.indexOf(e) != a.lastIndexOf(e));
-*/
--- CheckIO/02_Home/02_non-unique-elements/py-01/main.py
+++ CheckIO/02_Home/02_non-unique-elements/py-01/main.py
@@ -1,28 +1,22 @@
 # https://py.checkio.org/mission/non-unique-elements/solve/
 
-#Your optional code here
-#You can import some modules or create additional functions
-
-
 def checkio(data):
-    #Your code here
-    #It's main function. Don't remove this function
-    #It's used for auto-testing and must return a result for check.
+    for e in set(data):
+        idx = data.index(e)
+        try:
+            data.index(e, idx+1)
+        except:
+            data.remove(e)
 
-    #replace this for solution
     return data
 
-#Some hints
-#You can use list.count(element) method for counting.
-#Create new list with non-unique elements
-#Loop over original list
-
-
 if __name__ == "__main__":
-    #These "asserts" using only for self-checking and not necessary for auto-testing
-    assert isinstance(checkio([1]), list), "The result must be a list"
-    assert checkio([1, 2, 3, 1, 3]) == [1, 3, 1, 3], "1st example"
-    assert checkio([1, 2, 3, 4, 5]) == [], "2nd example"
-    assert checkio([5, 5, 5, 5, 5]) == [5, 5, 5, 5, 5], "3rd example"
-    assert checkio([10, 9, 10, 10, 9, 8]) == [10, 9, 10, 10, 9], "4th example"
+    ex1 = checkio([1, 2, 3, 1, 3]) # [1, 3, 1, 3], "1st example"
+    print(ex1)
+    ex2 = checkio([1, 2, 3, 4, 5]) # [], "2nd example"
+    print(ex2)
+    ex3 = checkio([5, 5, 5, 5, 5]) # [5, 5, 5, 5, 5], "3rd example"
+    print(ex3)
+    ex4 = checkio([10, 9, 10, 10, 9, 8]) # [10, 9, 10, 10, 9], "4th example"
+    print(ex4)
 
