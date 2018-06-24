156e2eec23788e978509c4b972e3464aa2b643bb
hopo <hpsight@gmail.com>
2018-06-24 20:53:36 +0900

    plp

--- Programmers/pylikepy/zip.py
+++ Programmers/pylikepy/zip.py
@@ -1,35 +1,38 @@
 # https://docs.python.org/3/library/functions.html?highlight=built%20function#zip
 
-#========
-mylist = [ [1,2,3], [4,5,6], [7,8,9] ]
-new_list = [[],[],[]]
+# ========
+mylist = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+new_list = [[], [], []]
 
 for i in range(3):
     for j in range(3):
-        new_list[i].append( mylist[j][i] )
+        new_list[i].append(mylist[j][i])
 
 
 # zip()
-mylist = [ [1,2,3], [4,5,6], [7,8,9] ]
+mylist = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 new_list = list(map(list, zip(*mylist)))
+print('new_list: ', new_list)
 
 
-#==========
-mylist = [ 1,2,3 ]
-new_list = [ 40, 50, 60 ]
+# A01)
+mylist = [1, 2, 3]
+new_list = [40, 50, 60]
+print('A01:')
 for i in zip(mylist, new_list):
     print (i)
 
-#ex1)
+# ex1)
 list1 = [1, 2, 3, 4]
 list2 = [100, 120, 30, 300]
 list3 = [392, 2, 33, 1]
 answer = []
+print('ex1: ')
 for i, j, k in zip(list1, list2, list3):
-   print( i + j + k )
+    print(i + j + k)
 
-#ex2)
+# ex2)
 animals = ['cat', 'dog', 'lion']
 sounds = ['meow', 'woof', 'roar']
 answer = dict(zip(animals, sounds))
-
+print('ex2: ', answer)
