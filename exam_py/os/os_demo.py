edd0dfd3b53b4d1b184b8f9dab4776f32eed21bc
hopo <hpsight@gmail.com>
2018-07-12 23:11:32 +0900

    .

--- exam_py/os/os_demo.py
+++ exam_py/os/os_demo.py
@@ -1,7 +1,8 @@
 import os
 
-ex1 = os.path.isdir("/Users/hp/.gdrive/list.txt") # dir
-ex2 = os.path.exists("/Users/hp/.gdrive/list.txt") # dir or file
+home = os.environ['HOME']
+ex1 = os.path.isdir(f'{home}/.python') # dir
+ex2 = os.path.exists(f'{home}/.python/list.txt') # dir or file
 
 print(ex1)
 print(ex2)
--- exam_py/scraping/scraping_ad.py
+++ exam_py/scraping/scraping_ad.py
@@ -2,7 +2,7 @@ import re
 import requests
 from bs4 import BeautifulSoup
 
-url = ''
+url = 'website.url'
 
 html = requests.get(url).text
 soup = BeautifulSoup(html)
