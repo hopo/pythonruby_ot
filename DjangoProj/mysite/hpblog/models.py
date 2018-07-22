bdd8ae15dffeab9f694d50a04eb7c072d17028d3
hopo <hpsight@gmail.com>
2018-07-22 15:13:28 +0900

    0

--- DjangoProj/mysite/hpblog/models.py
+++ DjangoProj/mysite/hpblog/models.py
@@ -4,7 +4,6 @@ from django.utils import timezone
 # Create your models here.
 
 class Post(models.Model):
-#     author = models.CharField(max_length=20)
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
