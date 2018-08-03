36d70e01acc4a6a0341862ff6b87c508dcef6ada
hopo <hpsight@gmail.com>
2018-08-04 00:07:03 +0900

    ,

--- DjangoProj/mysite/ref_app/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from markdownx.admin import MarkdownxModelAdmin
-from .models import MyModel
-
-# Register your models here.
-
-admin.site.register(MyModel, MarkdownxModelAdmin)
-
