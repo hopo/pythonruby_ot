ffd38b6e87ebf653a5ebdbf7c278c3195111913c
hopo <hpsight@gmail.com>
2018-08-05 17:50:24 +0900

    .

--- exam_py/flask_mongodb/run.py
+++ exam_py/flask_mongodb/run.py
@@ -3,9 +3,10 @@ from flask_mongoalchemy import MongoAlchemy
 
 app = Flask(__name__)
 app.config['MONGOALCHEMY_DATABASE'] = 'fla_db'
-app.config['MONGOALCHEMY_CONNECTION_STRING'] = 'mongodb://hpark:park@118.219.216.145:12717/fla_db'
+# app.config['MONGOALCHEMY_CONNECTION_STRING'] = 'mongodb://hpark:park@localhost:27017/fla_db'
 
 db = MongoAlchemy(app)
+
 
 class Author(db.Document):
     name = db.StringField()
