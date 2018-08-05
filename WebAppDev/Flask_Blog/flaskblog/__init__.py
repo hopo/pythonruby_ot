b3a312f8e370dd82a375169128c590570f1142b9
hopo <hpsight@gmail.com>
2018-08-05 17:57:01 +0900

    .

--- WebAppDev/Flask_Blog/flaskblog/__init__.py
+++ WebAppDev/Flask_Blog/flaskblog/__init__.py
@@ -33,3 +33,20 @@ login_manager.login_message_category = 'info'
 
 
 from flaskblog import routes
+
+
+# ########
+'''
+from flask import Flask
+from flask_mongoalchemy import MongoAlchemy
+
+app = Flask(__name__)
+app.config['MONGOALCHEMY_DATABASE'] = 'fla_db'
+# app.config['MONGOALCHEMY_CONNECTION_STRING'] = 'mongodb://hpark:park@localhost:27017/fla_db'
+
+db = MongoAlchemy(app)
+
+
+class Author(db.Document):
+    name = db.StringField()
+'''
