e009824c408448d6222829dd67709b6e85f7187b
hopo <hpsight@gmail.com>
2018-08-05 22:50:09 +0900

    .

--- WebAppDev/Django_Blog/mysite/settings.py
+++ WebAppDev/Django_Blog/mysite/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
 #         'ENGINE': 'django.db.backends.sqlite3',
         'ENGINE': 'djongo',
 #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'NAME': 'harpa_db',
+        'NAME': 'dj_db',
     }
 }
 
