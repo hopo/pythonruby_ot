45f1d29d38707d8efe25b275012089a000aec434
hopo <hpsight@gmail.com>
2018-09-12 21:33:51 +0900

    unittest

--- exam_py/unit-test/test_calc.py
+++ exam_py/unit-test/test_calc.py
@@ -1,8 +1,28 @@
 import unittest
 import calc
 
+# REF)
+# https://www.youtube.com/watch?v=6tNS--WetLI
 
 class TestCalc(unittest.TestCase):
+
+    # Before Test class
+    @classmethod
+    def setUpClass(cls):
+        print('setUpClass')
+
+    # After Test class
+    @classmethod
+    def tearDownClass(cls):
+        print('tearDownClass')
+
+    # Before Test function
+    def setUp(self):
+        pass
+
+    # After Test function
+    def tearDown(self):
+        pass
 
     def test_add(self):
         self.assertEqual(calc.add(10, 5), 15)
@@ -19,3 +39,4 @@ class TestCalc(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+
