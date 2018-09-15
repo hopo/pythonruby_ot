5349bf2b3230f35ec99d916aef7674443ecce989
hopo <hpsight@gmail.com>
2018-09-15 11:43:58 +0900

    .;

--- Programmers/forTest/stackque-truck02.py
+++ Programmers/forTest/stackque-truck02.py
@@ -7,21 +7,21 @@ def solution(bridge_length, weight, truck_weights):
     print("weight        : ", weight)
     print("truck_weights : ", truck_weights)
 
-    onbridge = 0
-    wait = 0
-    time = 0
-    i = 0
+    onbridge = list()
+    cnt = 0
 
-    for tw in truck_weights:
-        time += 1
-        onbridge += tw
-        if onbridge > weight:
-            wait += 1
-        if time % bridge_length == 0:
-            onbridge -= truck_weights[i]
-            i += 1
+    for w in truck_weights:
 
-    return bridge_length + time + wait
+        onbridge.append(w)
+        if sum(onbridge) > weight:
+            cnt += 1
+
+        if len(onbridge) == bridge_length:
+            onbridge.pop(0)
+
+    ret = len(truck_weights)*bridge_length + bridge_length - cnt
+
+    return ret
 
 
 bridge_length = [2, 100, 100]
@@ -34,8 +34,7 @@ result        = [8, 101, 110]
 
 # checker
 
-for i in [0, 1, 2]:
+for i in range(2, 3):
     test = solution(bridge_length[i], weight[i], truck_weights[i])
     print(f'test{i}: {result[i]} == {test} ?', result[i] == test)
     print()
-
--- Programmers/forTest/stackque-truck03.py
+++ Programmers/forTest/stackque-truck03.py
@@ -7,21 +7,28 @@ def solution(bridge_length, weight, truck_weights):
     print("weight        : ", weight)
     print("truck_weights : ", truck_weights)
 
+    ret = (bridge_length + 1)
+
     onbridge = 0
     wait = 0
     time = 0
     i = 0
+    cnt = 0
 
     for tw in truck_weights:
         time += 1
-        onbridge += tw
         if onbridge > weight:
             wait += 1
-        if time % bridge_length == 0:
+        else:
+            onbridge += tw
+            cnt += 1
+
+        if cnt > bridge_length:
+            cnt = 0
             onbridge -= truck_weights[i]
             i += 1
 
-    return bridge_length + time + wait
+    return ret + time + wait
 
 
 bridge_length = [2, 100, 100]
@@ -34,7 +41,7 @@ result        = [8, 101, 110]
 
 # checker
 
-for i in [0, 1, 2]:
+for i in [0]:
     test = solution(bridge_length[i], weight[i], truck_weights[i])
     print(f'test{i}: {result[i]} == {test} ?', result[i] == test)
     print()
