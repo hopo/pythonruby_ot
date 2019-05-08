c4be9207c24ed42370b33b77e29e85e3acdc9f64
hopo <hpsight@gmail.com>
2019-05-09 01:37:47 +0900

    CIO home

---
+++ CheckIO/02_Home/time-converter/solution.py
@@ -0,0 +1,20 @@
+# ing...
+
+def time_converter(time):
+    apm = 'a.m.'
+    hour = int(time[:2])
+    if hour > 12:
+        hour = hour-12
+        apm = 'p.m.'
+
+    return '{}{} {}'.format(hour, time[2:], apm)
+
+if __name__ == '__main__':
+    print("Example:")
+    print(time_converter('12:30'))
+
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert time_converter('12:30') == '12:30 p.m.'
+    assert time_converter('09:00') == '9:00 a.m.'
+    assert time_converter('23:15') == '11:15 p.m.'
+    print("Coding complete? Click 'Check' to earn cool rewards!")
